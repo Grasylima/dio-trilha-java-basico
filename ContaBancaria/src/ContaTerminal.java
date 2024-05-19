c60e23bebec96b3524df15b6c934daf0f1fa6836
Grasylima <grasiellelima.ti@gmail.com>
2024-05-19 18:43:07 -0300

    atualizacao

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -1,5 +1,16 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        String agencia = args [0];
+        String nomeCliente = args [1];
+        int  numeroConta = Integer.parseInt(args[2]);
+        double saldo = Double.parseDouble(args[3]);
+
+        System.out.println (" Meu nome é " + nomeCliente);
+        System.out.println (" Minha agencia é " + agencia + " " );
+        System.out.println (" Numero da Conta é " + numeroConta + " " );
+        System.out.println (" Meu saldo é " + saldo + " ");
+
+
     }
 }
